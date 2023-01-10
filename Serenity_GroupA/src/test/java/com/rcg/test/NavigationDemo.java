e897274cb8cd63dd61a21b423a744ab48a431c99
JVSiytoo <JVSiytoo@F04304.rcgitmanila.com>
2023-01-10 22:24:51 +0800

    SIYTOO-Added a test case

---
+++ Serenity_GroupA/src/test/java/com/rcg/test/NavigationDemo.java
@@ -0,0 +1,9 @@
+package com.rcg.test;
+
+import net.serenitybdd.core.steps.UIInteractions;
+
+public class NavigationDemo extends UIInteractions {
+	public void SearchPage() {
+		openUrl("https://www.google.com/");
+	}
+}
