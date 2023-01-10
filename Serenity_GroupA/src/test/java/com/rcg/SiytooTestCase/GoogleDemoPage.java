e4d66f84d17110cd23cdba4190edfea75e329500
JVSiytoo <JVSiytoo@F04304.rcgitmanila.com>
2023-01-10 22:43:32 +0800

    created a new package for my test case

---
+++ Serenity_GroupA/src/test/java/com/rcg/SiytooTestCase/GoogleDemoPage.java
@@ -0,0 +1,19 @@
+package com.rcg.SiytooTestCase;
+
+import org.openqa.selenium.By;
+
+import net.serenitybdd.core.steps.UIInteractions;
+
+public class GoogleDemoPage extends UIInteractions {
+	private static final By googleImages = By.xpath("//a[text()='Images']");
+	private static final By verification = By.xpath("//span[text()='Images']");
+
+	public void webRedirect() {
+		$(googleImages).click();
+	}
+
+	public String confirmTrue() {
+		return $(verification).getText();
+
+	}
+}
