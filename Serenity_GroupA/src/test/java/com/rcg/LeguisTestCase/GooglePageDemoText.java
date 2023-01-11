b92177f30be683055119384d74b8e3c415cbaf23
JVSiytoo <JVSiytoo@F04304.rcgitmanila.com>
2023-01-11 21:08:07 +0800

    Converted to JUnit5

--- Serenity_GroupA/src/test/java/com/rcg/LeguisTestCase/GooglePageDemoText.java
+++ Serenity_GroupA/src/test/java/com/rcg/LeguisTestCase/GooglePageDemoText.java
@@ -2,7 +2,7 @@ package com.rcg.LeguisTestCase;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
