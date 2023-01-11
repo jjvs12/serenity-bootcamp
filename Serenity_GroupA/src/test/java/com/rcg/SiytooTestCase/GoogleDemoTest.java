094f23317a78e911f41831386d3f3b28cb11635f
JVSiytoo <JVSiytoo@F04304.rcgitmanila.com>
2023-01-12 00:37:20 +0800

    Converted to JUnit5 to run the brunch of LEGUIS

--- Serenity_GroupA/src/test/java/com/rcg/SiytooTestCase/GoogleDemoTest.java
+++ Serenity_GroupA/src/test/java/com/rcg/SiytooTestCase/GoogleDemoTest.java
@@ -1,7 +1,7 @@
 package com.rcg.SiytooTestCase;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
