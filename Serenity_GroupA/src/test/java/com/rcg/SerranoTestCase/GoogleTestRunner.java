0517aa5f8bbee6c3d25d3b6460eca5b5f9ef0908
JVSiytoo <JVSiytoo@F04304.rcgitmanila.com>
2023-01-11 21:01:27 +0800

    Edited the imports

--- Serenity_GroupA/src/test/java/com/rcg/SerranoTestCase/GoogleTestRunner.java
+++ Serenity_GroupA/src/test/java/com/rcg/SerranoTestCase/GoogleTestRunner.java
@@ -3,7 +3,7 @@ package com.rcg.SerranoTestCase;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
